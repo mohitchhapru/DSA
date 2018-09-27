46c4eb89d81ac01ec2f01ee4c80ee6b62d571298
Mohit Chhapru <chhaprumohit@gmail.com>
2018-09-26 20:35:11 -0700

    Question Added

---
+++ DynamicProgramming/budgetShopping.java
@@ -0,0 +1,21 @@
+/**
+ * Helen's school has provided her with a budget for purchasing the math notebooks her students will need.
+ * There are several stores that sells the bundles of notebooks for various prices. 
+ * she can only purhcase full bundle.
+ * 
+ * Determine the maximum number of notebooks Helen can purchase with the amount she is given.
+ * 
+ * For example: If Helen has $50 and bundle of 20 notebooks each cost $12, she can buy 4 bundles for $48 
+ * and $2 leftover and has purchased 80 notebooks.
+ * 
+ * Budget Shopping has the following paramaters:
+ *  n: integer, the number of dollars in Helen's notebook budget
+ *  bundleQuantities: integer array, int number of notebooks in a bundle at shop[i]
+ *  bundleCosts: integer array, the cost of a bundle of notebooks at shop[i]
+ * 
+ * Constraints:
+ * 1 <= n <= 10^4
+ * 1 <= m <= 10^2
+ * 1 <= bundleQuantities[i] <= 20
+ * 1 <= bundleCost[i] <= 200
+ */
