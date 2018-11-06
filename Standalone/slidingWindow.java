ec55f86e29f495e52bafa55a8eacc98753090e40
Mohit Chhapru <chhaprumohit@gmail.com>
2018-11-05 21:47:46 -0800

    Found the max array of sliding window k

---
+++ Standalone/slidingWindow.java
@@ -0,0 +1,62 @@
+/**
+ * Sliding Window Maximum
+ * 
+ * Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
+ * 
+ * Example:
+ * 
+ *  Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+ * Output: [3,3,5,5,6,7] 
+ * Explanation: 
+ * 
+ * Window position                Max
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ *  1 [3  -1  -3] 5  3  6  7       3
+ *  1  3 [-1  -3  5] 3  6  7       5
+ *  1  3  -1 [-3  5  3] 6  7       5
+ *  1  3  -1  -3 [5  3  6] 7       6
+ *  1  3  -1  -3  5 [3  6  7]      7
+ * 
+ * Note: 
+ * You may assume k is always valid, 1 ≤ k ≤ input array's size for non-empty array.
+ * 
+ * Follow up:
+ * Could you solve it in linear time?
+ * 
+ * Yes, This is been solved using deque (Ref from GeeksforGeeks)
+ */
+
+class slidingWindow {
+    public int[] maxSlidingWindow(int[] nums, int k) {  
+        if(nums.length == 0 || k == 0)
+            return new int[]{};
+        
+        int[] maxArr = new int[nums.length+1-k];
+        int maxArrCounter = 0;
+        
+        Deque<Integer> dq = new LinkedList<Integer>();
+        
+        for(int i=0; i<k; i++){
+            while(!dq.isEmpty() && nums[i] >= nums[dq.peekLast()])
+                dq.removeLast();
+            dq.addLast(i);
+        }
+                       
+        
+        for(int i=k;i<nums.length;i++){
+            
+            maxArr[maxArrCounter++] = nums[dq.peekFirst()];
+            
+            while(!dq.isEmpty() && dq.peekFirst() <= i-k)
+                dq.removeFirst();
+            
+            while(!dq.isEmpty() && nums[i] >= nums[dq.peekLast()])
+                dq.removeLast();
+            
+            dq.addLast(i);            
+        }
+        maxArr[maxArrCounter++] = nums[dq.peekFirst()];
+        return maxArr;
+    }
+}
